99a3fb49f1f4d4c13768e6e42659032a8dc9b4c3
sidpd57 <sidpd57@gmail.com>
2023-09-28 19:10:48 +0530

    add-hero-and-cta-to-marketing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignIn from "./components/auth/SignIn"
 import SignUp from "./components/auth/SignUp"
 import Hero from "./components/marketing/Hero"
 import Cta from "./components/marketing/Cta"
+import Footer from "./components/marketing/Footer"
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <>
       <Navbar/>
       <Hero/>
-      
+      <Footer/>
     </>
   )
 }
