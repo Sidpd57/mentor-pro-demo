cf30ec54e55ae36bb395239d5b65058477916f6e
sidpd57 <sidpd57@gmail.com>
2023-10-04 11:13:58 +0530

    add index.js & remove custom style from index.css

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Header from './Header/Header'
+import Home from './Home/Home'
+import Footer from './Footer/Footer'
+import Hero from './Ui-components/Hero'
+import Pricing from './Ui-components/Pricing'
+import Testimonial from './Ui-components/Testimonial'
+import Contact from './Contact/Contact'
+import About from './About/About'
+import Login from './Auth/Login'
+import Signup from './Auth/Signup'
+
+
+
+export { Header, Home , Footer , Hero, Pricing, Testimonial, Contact, About, Login, Signup}
+
