68bcddfee4de5424c687ff846626efc0752a513a
sidpd57 <sidpd57@gmail.com>
2023-10-04 11:12:57 +0530

    add layout & config main for react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './Layout'
 import './index.css'
+import { Route, RouterProvider } from 'react-router-dom'
+import { Home, About, Contact, Login, Signup } from './components'
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='signin' element={<Signup/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
