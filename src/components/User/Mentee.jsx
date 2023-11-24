ce6993914f511fba18fc487b8d97be019163132c
sidpd57 <sidpd57@gmail.com>
2023-11-24 13:37:58 +0530

    add mentee profile cards

--- src/components/User/Mentee.jsx
+++ src/components/User/Mentee.jsx
@@ -1,48 +1,23 @@
 import React from 'react'
+import mentee from '../mentee';
+import {ProfileCard} from '../index'
+
+function CreateCard(mentee){
+  return (
+    <ProfileCard
+      key = {mentee.id}
+      name = {mentee.name}
+      img = {mentee.imgUrl}
+      description = {mentee.description}
+    />
+  );
+}
 
 function Mentee() {
     return (
-      <section class="px-2 py-10 md:px-0">
-      <figure class="mx-auto max-w-2xl">
-        <h1 class="mb-4 text-4xl font-semibold text-black">
-          What developers are saying ?
-        </h1>
-        <blockquote class="mt-10 text-xl text-gray-900">
-          <p>
-            “Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
-            perspiciatis quam nulla, rerum provident dicta ipsum officiis nobis
-            dolore neque commodi praesentium placeat earum assumenda.”
-          </p>
-        </blockquote>
-        <figcaption class="mt-10 flex items-center gap-x-6">
-          <div class="isolate flex -space-x-2">
-            <img
-              class="relative z-30 inline-block h-10 w-10 rounded-full ring-2 ring-white"
-              src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
-              alt="Dan_Abromov"
-            />
-            <img
-              class="relative z-20 inline-block h-10 w-10 rounded-full ring-2 ring-white"
-              src="https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_170,w_170,f_auto,g_faces,z_0.7,b_white,q_auto:eco,dpr_1/smokhfs2uevnppc2bmwl"
-              alt="Guillermo_Rauch"
-            />
-            <img
-              class="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white"
-              src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&amp;w=256&amp;q=75"
-              alt="Lee_Robinson"
-            />
-            <img
-              class="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white"
-              src="https://nextjs.org/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F35255%2F1665059775-delba.jpg&amp;w=640&amp;q=75"
-              alt="Delba"
-            />
-          </div>
-          <div>
-            <p class="font-semibold text-black">200+ developers</p>
-          </div>
-        </figcaption>
-      </figure>
-    </section>
+      <div className='p-5 px-11 grid grid-cols-4 gap-10 py-5'>
+        {mentee.map(CreateCard)}
+      </div>
     
     )
 }
---
+++ src/components/mentee.js
@@ -0,0 +1,40 @@
+const mentee = [
+    {
+        id: 1,
+        name: "Janko Ferlič",
+        imgUrl: "https://images.unsplash.com/photo-1595760780346-f972eb49709f?q=80&w=2927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+    {
+        id: 2,
+        name: "Reynardo Etenia Wongso",
+        imgUrl: "https://images.unsplash.com/photo-1611146033545-5e1e5ad951d8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+    {
+        id: 3,
+        name: "Amanda Sofia Pellenz",
+        imgUrl: "https://images.unsplash.com/photo-1593183230686-69876b0cb240?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+    {
+        id: 4,
+        name: "Aedrian",
+        imgUrl: "https://images.unsplash.com/photo-1615863712239-becde744591a?q=80&w=3136&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+    {
+        id: 5,
+        name: "Michael Mims",
+        imgUrl: "https://images.unsplash.com/photo-1473280025148-643f9b0cbac2?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+    {
+        id: 6,
+        name: "Julian Scagliola",
+        imgUrl: "https://images.unsplash.com/photo-1595281277591-9c58828b40bf?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?"
+    },
+]
+
+export default mentee 
